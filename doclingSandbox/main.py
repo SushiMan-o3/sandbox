7333edb1b00476ee4c0805c0d1d3eec4925ab52b
Sushim Malla <sushim.m17@gmail.com>
2026-06-27 01:34:28 -0400

    is able to read pdfs and images with normal text

---
+++ doclingSandbox/main.py
@@ -0,0 +1,57 @@
+import shutil
+import tempfile
+from pathlib import Path
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.datamodel.pipeline_options import PdfPipelineOptions, EasyOcrOptions
+from docling.datamodel.base_models import InputFormat
+
+# --- schemas --- 
+
+class GetImgResponse(BaseModel):
+    name: str
+    markdown: str
+    
+
+# --- setup for functions + functions ---
+
+pipeline_options = PdfPipelineOptions()
+pipeline_options.do_ocr = True
+pipeline_options.ocr_options = EasyOcrOptions()
+
+converter = DocumentConverter(
+    format_options={
+        InputFormat.IMAGE: PdfFormatOption(pipeline_options=pipeline_options),
+        InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options),
+    }
+)
+
+
+def convert_to_markdown(upload: UploadFile):
+    suffix = Path(upload.filename).suffix
+    with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
+        shutil.copyfileobj(upload.file, tmp)
+        tmp_path = tmp.name
+
+    try:
+        doc = converter.convert(tmp_path).document
+        return doc.export_to_markdown()
+    finally:
+        Path(tmp_path).unlink(missing_ok=True)
+
+# --- fastAPI set-up + Routes --- 
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/upload_file_to_markdown", response_model=GetImgResponse)
+def upload_file_to_markdown(name: str, file: UploadFile = File(...)):
+    return {
+        "name": name,
+        "markdown": convert_to_markdown(file)
+    }
---
+++ doclingSandbox/notes.md
@@ -0,0 +1,4 @@
+# docling is veryyyy slowww
+- even when it fails to find the file itself it is very slow
+- looked into other readers but they all cost money + the free one didnt work 
+- also hand written notes dont work as well but ocr pdfs and images work fine
