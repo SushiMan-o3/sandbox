6719c47810b75dbe6cb44c8a4c7ee214a268319a
Sushim Malla <sushim.m17@gmail.com>
2026-06-30 19:50:13 -0400

    updated the url

--- test_proj/frontend/vite.config.js
+++ test_proj/frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://recipevault-proto.vercel.app/',
+        target: 'https://recipevault-proto.vercel.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
